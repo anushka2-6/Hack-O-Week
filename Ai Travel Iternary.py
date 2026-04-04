a79256cc4ec6121ba8899cb90f7f1f5f21c89fd2
Anushka Malose <anushkasandipmalose@gmail.com>
2026-04-04 12:17:37 +0530

    Create Ai Travel Iternary.py

---
+++ Ai Travel Iternary.py
@@ -0,0 +1,17 @@
+DATA = {
+    "goa": {
+        "beaches": ["Baga Beach", "Calangute Beach", "Anjuna Beach"],
+        "nightlife": ["Tito’s Lane", "Club Cubana"],
+        "food": ["Beach shacks", "Fish thali"],
+        "nature": ["Dudhsagar Falls"],
+        "culture": ["Fort Aguada", "Basilica of Bom Jesus"]
+    },
+
+    "mumbai": {
+        "beaches": ["Juhu Beach", "Girgaum Chowpatty"],
+        "nightlife": ["Marine Drive"],
+        "food": ["Street food"],
+        "nature": ["Sanjay Gandhi National Park"],
+        "culture": ["Gateway of India"]
+    }
+}
