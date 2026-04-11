4b4f040984ec944e78bfc59e874c96f8103f70ec
Anushka Malose <anushkasandipmalose@gmail.com>
2026-04-11 12:50:05 +0530

    Update utils.py

--- Travel Planner/utils.py
+++ Travel Planner/utils.py
@@ -1,1 +1,14 @@
+import re
+import string
 
+stopwords = {
+    "is", "the", "a", "an", "and", "or", "in", "on", "at",
+    "to", "for", "of", "with", "by", "what", "how", "why"
+}
+
+def preprocess(text):
+    text = text.lower()
+    text = text.translate(str.maketrans('', '', string.punctuation))
+    tokens = re.findall(r'\b\w+\b', text)
+    tokens = [word for word in tokens if word not in stopwords]
+    return tokens
