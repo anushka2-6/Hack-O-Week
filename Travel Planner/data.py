5fe03d952037dec550dd9070826aca22d04f7eff
Anushka Malose <anushkasandipmalose@gmail.com>
2026-04-11 12:48:55 +0530

    Update data.py

--- Travel Planner/data.py
+++ Travel Planner/data.py
@@ -1,1 +1,22 @@
-
+destinations = {
+    "Manali": {
+        "best_time": "October to February",
+        "budget": "₹10,000 - ₹20,000",
+        "famous": "Snow, Mountains"
+    },
+    "Goa": {
+        "best_time": "November to March",
+        "budget": "₹8,000 - ₹15,000",
+        "famous": "Beaches, Nightlife"
+    },
+    "Mumbai": {
+        "best_time": "October to March",
+        "budget": "₹5,000 - ₹12,000",
+        "famous": "City life, Marine Drive"
+    },
+    "Shimla": {
+        "best_time": "December to February",
+        "budget": "₹9,000 - ₹18,000",
+        "famous": "Snow, Hills"
+    }
+}
